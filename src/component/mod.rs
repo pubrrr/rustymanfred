3a7a24883de0b7ace6e3450d4bedb7f6dbddcec2
pubrrr <62142301+pubrrr@users.noreply.github.com>
2021-11-12 17:39:36 +0100

    manfred can now run to the left

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,12 +1,23 @@
 pub mod manfred;
 
 pub struct Position {
-    pub x: u32,
-    pub y: u32,
+    pub x: i32,
+    pub y: i32,
 }
 
 impl Position {
-    pub fn new(x: u32, y: u32) -> Position {
+    pub fn new(x: i32, y: i32) -> Position {
         Position { x, y }
+    }
+}
+
+pub struct Velocity {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Velocity {
+    pub fn new() -> Velocity {
+        Velocity { x: 0, y: 0 }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
-mod component;
+use std::ops::Deref;
+
+use bevy::core::CorePlugin;
+use bevy::input::InputPlugin;
+use bevy::prelude::*;
+use bevy::window::WindowPlugin;
 
 use crate::component::manfred::Manfred;
-use crate::component::Position;
-use bevy::prelude::*;
+use crate::component::{Position, Velocity};
+
+mod component;
 
 fn main() {
     App::build()
+        .add_plugins(DefaultPlugins)
         .add_startup_system(add_manf.system())
-        .add_system(hello_system.system())
-        .add_system(component.system())
+        .add_system(velocity_components.system())
+        .add_system(position_components.system())
         .run();
 }
 
-fn hello_system() {
-    println!("hi");
-}
-
 fn add_manf(mut commands: Commands) {
-    commands.spawn().insert(Manfred).insert(Position::new(0, 1));
+    commands.spawn_bundle((Manfred, Position::new(0, 0), Velocity::new()));
 }
 
-fn component(query: Query<&Position, With<Manfred>>) {
-    println!("query arrived: {}", query.iter().count());
-    query.for_each(|position| println!("manfred at ({},{})", position.x, position.y));
+fn position_components(mut query: Query<(&mut Position, &Velocity), With<Manfred>>) {
+    println!("position query arrived: {}", query.iter_mut().count());
+    query.for_each_mut(|(mut position, velocity)| {
+        position.x += velocity.x;
+        position.y += velocity.y;
+        println!("manfred at ({},{})", position.x, position.y)
+    });
+}
+
+fn velocity_components(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Velocity, With<Manfred>>,
+) {
+    query.for_each_mut(|mut velocity| {
+        if keyboard_input.pressed(KeyCode::A) {
+            velocity.x -= 1;
+        }
+    });
 }
