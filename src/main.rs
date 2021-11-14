4cb999e9fd0f168333bc77ab7a4c1e953e372cbd
pubrrr <62142301+pubrrr@users.noreply.github.com>
2021-11-14 21:17:26 +0100

    display Manni's ugly face

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,23 @@ fn main() {
         .run();
 }
 
-fn add_manf(mut commands: Commands) {
+fn add_manf(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load("images\\manfred_down0.png");
     commands
         .spawn_bundle((Manfred, Position::new(0, 0), Velocity::new()))
         .insert_bundle(SpriteBundle {
-            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            sprite: Sprite::new(Vec2::new(40.0, 80.0)),
+            material: materials.add(texture_handle.into()),
             ..Default::default()
         });
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
 fn position_components(mut query: Query<(&mut Transform, &Velocity)>) {
-    println!("position query arrived: {}", query.iter_mut().count());
     query.for_each_mut(|(mut transform, velocity)| {
         transform.translation.x += velocity.x as f32;
         transform.translation.y += velocity.y as f32;
